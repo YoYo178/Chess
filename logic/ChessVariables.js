de6b549d13086083b15e30ee30fde58acfefe768
-_YoYo178_- <95132723+YoYo178@users.noreply.github.com>
2025-05-04 13:06:25 +0530

    Update API URL

--- logic/ChessVariables.js
+++ logic/ChessVariables.js
@@ -22,4 +22,4 @@ export const CHESS_SFX = {
 	CHECK: new Audio("")
 }
 
-export const CHESS_SERVER_URL = "https://chess-api.duckdns.org/logic/api"
+export const CHESS_SERVER_URL = "https://sp-az.duckdns.org/chess/api"
